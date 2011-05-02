d07a78de496d07b0a822d759df5487a0e7abf5d3
Ben Nadel <ben@bennadel.com>
2011-05-02 09:19:05 -0400

    Updated the Application.js with some more demo code. Also, touched the response proxy to clean up a tiny bit of logic.

--- Application.js
+++ Application.js
@@ -1,29 +1,41 @@
 
 // Define the factory object for Application.js. Since each incoming 
 // request has to define a new instance of the Application.js, we 
-// have to get around the module-based caching.
+// have to get around the module-based caching. This gives every 
+// request the opportunity to re-define the settings (this is a good
+// thing - and a very powerful thing).
 module.exports = function( request, response ){
 
 
 	// Define the application settings.
 	this.name = "ColdFusion.js Test App";
 	
-	// I am the amount of time (in seconds) the application can sit idle.
-	this.applicationTimeout = 10;
+	// I am the amount of time (in seconds) the application can 
+	// sit idle before it timesout. Once timing out, the 
+	// onApplicationEnd() event handler will be implicitly invoked,
+	// and any subsequent request will boot up a new application. 
+	this.applicationTimeout = 15;
 	
-	// I determine whether or not session management is being used for 
-	// this page request. This can be turned on or off for each request.
+	// I determine whether or not session management is being used 
+	// for this page request. This can be turned on or off for each
+	// request.
 	this.sessionManagement = true;
 	
-	// I am the amount of time (in seconds) the session can sit idle.
+	// I am the amount of time (in seconds) the session can sit idle
+	// before it timesout. Once timing out, the onSessionEnd() event
+	// handler is implicitly invoked, and any subsequent request will
+	// boot up a new session.
 	this.sessionTimeout = 5;
 	
-	// I determine if the session cookies should be set automatically.
-	// If false, we will need to manually set the cookies.
+	// I determine if the session cookies should be set 
+	// automatically by the framework. If true, a sessionID cookie
+	// will be sent back to the browser. If false, the cookie needs
+	// to be manually set.
 	this.setClientCookies = true;
 	
-	// I am the amount of time the request can run before it is forced to
-	// end (optional - exclude this for an open-ended response).
+	// I am the amount of time the request can run before it is 
+	// forced to end (optional - exclude this property for an open-
+	// ended response time).
 	this.requestTimeout = 5;
 
 
@@ -34,89 +46,106 @@ module.exports = function( request, response ){
 	// I initialize the application.
 	this.onApplicationStart = function( request, response, callback ){
 		
-		console.log( "[Application Start]" );
+		// Log event handler.
+		console.log( ">> Event :: onApplicationStart()" );
+		console.log( ">>>> Application Name :: " + request.application.getName() );
 		
-		// Indicate that we want the application to load.
+		// Set an application value.
+		request.application.set( "message", "ColdFusion FTW!!" );
+		
+		// Return true so the rest of the application can load.
 		return( callback( true ) );
 		
-	}
+	};
 
 
 	// I initialize the session.
 	this.onSessionStart = function( request, response, callback ){
+
+		// Log event handler.
+		console.log( ">> Event :: onSessionStart()" );
+		console.log( ">>>> Session ID :: " + request.session.getSessionID() );
 		
-		console.log( "[Session Start]" );
-		console.log( "Session ID: " + request.session.getSessionID() );
-		
+		// Store a session value.
 		request.session.set( "hitCount", 0 );
 		
-		response.cookies.set( "foo", "bar" );
-		
+		// Return out so the framework knows the event is over.
 		return( callback() );
 		
-	}
+	};
 
 
 	// I initialize the request.
 	this.onRequestStart = function( request, response, callback ){
 		
-		console.log( "[Request Start]" );
+		// Log event handler.
+		console.log( ">> Event :: onRequestStart()" );
 		
+		// Increment the session-based hit-count. Notice that once a
+		// value is stored in the session cache, it can be referenced
+		// without a getter / setter.
 		request.session.hitCount++;
-		
-		if (request.session.hitCount == 4){
-
-			response.cookies.remove( "foo" );
-
-		}
-		
-		request.session.set( "blam", "blammy" );
-		request.session.blam = "floozy";
-		request.session.remove( "blam" );
-		
-		
-		// Indicate that we want the request to load.
+				
+		// Return true so the rest of the request can load.
 		return( callback( true ) );
 		
-	}
+	};
 
 
 	// I process the request.
 	this.onRequest = function( request, response ){
 		
-		console.log( "[Request]" );
+		// Log event handler.
+		console.log( ">> Event :: onRequest()" );
 		
+		// Set the content type.
 		response.setHeader( "content-type", "text/html" );
-		response.write( "<h1>ColdFusion.js On Node.js</h1>" );
-		response.write( "<p>Hello - this is page request " + request.session.get( "hitCount" ) + ".</p>" );
-		response.end( "<p>Word up!!.</p>" );
+		
+		// Write out some content.
+		response.write( 
+			"<h1>ColdFusion.js On Node.js</h1>" + 
+			"<p>Hello - this is page request " + 
+				request.session.hitCount +  
+			".</p>"
+		);
+		
+		// End the response.
+		response.end( 
+			"<p>" + 
+				request.application.message + 
+			"</p>"
+		);
 			
-	}
+	};
 	
 	
 	// I tear down the request.
 	this.onRequestEnd = function( request, response ){
 		
-		console.log( "[Request End]" );
+		// Log event handler.
+		console.log( ">> Event :: onRequestEnd()" );
 		
-	}
+	};
 
 	
 	// I tear down the session.
 	this.onSessionEnd = function( applicationScope, sessionScope ){
 		
-		console.log( "[Session End]" );
-		console.log( "Session ID: " + sessionScope.getSessionID() );
+		// Log event handler.
+		console.log( ">> Event :: onSessionEnd()" );
+		console.log( ">>>> Session ID :: " + sessionScope.getSessionID() );
 		
-	}
+	};
 
 
 	// I tear down the application.
 	this.onApplicationEnd = function( applicationScope ){
 		
-		console.log( "[Application End]" );
+		// Log event handler.
+		console.log( ">> Event :: onApplicationEnd()" );
+		console.log( ">>>> Application Name :: " + applicationScope.getName() );
 		
-	}
+	};
 	
 	
 	// I handle any global errors. 
@@ -125,20 +154,25 @@ module.exports = function( request, response ){
 	// the error occurred during an asynchronous callback, this 
 	// Application.js instance might not even be the one that started
 	// the action that ended up raising the exception.
-	this._onError = function( error, request, response ){
+	this.onError = function( error, request, response ){
 		
-		console.log( "Uh oh! an error occurred!" );
+		// Log event handler.
+		console.log( ">> Event :: onError()" );
+		console.log( ">>>> Error Message :: " + error.message );
 		
 		// Check to see if there is a response associated with this
-		// error. 
-		if (response){
+		// error is available and is not committed.
+		if (
+			response &&
+			!response.isCommitted()
+			){
 			
 			// Output the error.
-			response.write( "An error has occurred: " + error.message );
+			response.write( "<p>An error has occurred: " + error.message + ".</p>" );
 
 		}
 		
-	}
+	};
 	
 	
 };
